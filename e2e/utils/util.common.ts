4f3628b648365b1d2da3c26640e8c051961d75b7
Khobragade, Swapnil <swapnil_khobragade_tp@bmc.com>
2019-11-15 16:13:33 +0530

    added utility method for dropdown select (#6)

--- e2e/utils/util.common.ts
+++ e2e/utils/util.common.ts
@@ -1,7 +1,13 @@
-import { browser, until, ExpectedConditions, element, by, By } from 'protractor';
+import { browser, until, ExpectedConditions, element, by, By, $,$$, ProtractorExpectedConditions, protractor } from 'protractor';
 import { WebDriverLocator } from 'protractor/built/locators';
 
 export class Util{
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors= {
+        dropdownBox: '.ui-select-match',
+        valueSearch: ' input[type="search"]',
+        dropDownOption: '.ui-select__rx-choice',
+    }
 
     async clickOnElement(locator: WebDriverLocator){
         const webElement = await browser.wait(until.elementLocated(locator), 10000);
@@ -14,4 +20,21 @@ export class Util{
         await browser.wait(ExpectedConditions.invisibilityOf(element(by.xpath("//*[contains(@class,'d-preloader ')]"))));
 //        await browser.wait(until.elementIsNotVisible(element(by.xpath("//*[contains(@class,'d-preloader ')]"))), 5000);
     }
+
+    async selectDropDownOptionWithValueEntered(guid:string, value:string){        
+        const dropDown =await $(`[rx-view-component-id="${guid}"]`);
+        await browser.wait(this.EC.elementToBeClickable(dropDown.$(this.selectors.dropdownBox)));
+        await dropDown.$(this.selectors.dropdownBox).click();
+        await browser.wait(this.EC.elementToBeClickable(dropDown.$(this.selectors.valueSearch)));
+        await dropDown.$(this.selectors.valueSearch).sendKeys(value);
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await $$(this.selectors.dropDownOption).count();
+            return count >= 1;
+    }))
+    await browser.wait(this.EC.elementToBeClickable(element(by.cssContainingText(this.selectors.dropDownOption, value))));
+    await element(by.cssContainingText(this.selectors.dropDownOption, value)).click();
+  }
+
 }
+
+export default new Util();
