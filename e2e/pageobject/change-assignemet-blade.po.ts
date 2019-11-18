39bd02e58a78a4c6e1b631faf4faad6fc8dd428d
Tidke, Pravin <pravin_tidke_tp@bmc.com>
2019-11-18 11:34:38 +0530

    assignment blade (#7)
    * assignment blade
    
    * review changes

---
+++ e2e/pageobject/change-assignemet-blade.po.ts
@@ -0,0 +1,93 @@
+import {by, $, $$, ProtractorExpectedConditions, browser, protractor, element } from "protractor";
+class changeAssignmentBlade{
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors = {
+        changeAssignmentButton: '[rx-view-component-id="6041cce1-05bd-458d-b097-eb310507cae3"] button',
+        assignButton: '.rx-assignment-modal-footer button.d-button_primary',
+        assignmentDropDownList: '.rx-assignment_modal_filters .rx-assignment-select',
+        selectOptions: '.options-box .options li',
+        cancelButton: '.rx-assignment-modal-footer button.d-button_secondary',
+        assignToMeCheckBox: '.rx-assignment_assignToMe span',
+        searchAsignee: '.d-icon-search input',
+        assignee: '.rx-assignment-person-fullName',
+    }
+
+    async clickOnAssignButton():Promise<void>{
+        await browser.wait(this.EC.visibilityOf($(this.selectors.assignButton)));
+        await $(this.selectors.assignButton).click();
+    }
+
+    async clickOnAssignToMeCheckBox():Promise<void>{
+        await browser.wait(this.EC.visibilityOf($(this.selectors.assignToMeCheckBox)));
+        await $(this.selectors.assignToMeCheckBox).click();
+    }
+
+    async selectCompany(companyValue:string): Promise<void> {
+        const companyDropDown = $$(this.selectors.assignmentDropDownList).get(0);
+        await browser.wait(this.EC.elementToBeClickable(companyDropDown.$('button')));
+        await companyDropDown.$('button').click();
+        await browser.wait(this.EC.visibilityOf(companyDropDown.$('input')));
+        await companyDropDown.$('input').sendKeys(companyValue);
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await companyDropDown.$$(this.selectors.selectOptions).count();
+            return count >= 1;
+        }))
+        await browser.wait(this.EC.elementToBeClickable(element(by.cssContainingText(this.selectors.selectOptions, companyValue))));
+        await element(by.cssContainingText(this.selectors.selectOptions, companyValue)).click();
+    }
+
+    async selectBusinessUnit(businessUnit:string): Promise<void> {
+        const businessUnitDropDown = $$(this.selectors.assignmentDropDownList).get(1);
+        await browser.wait(this.EC.elementToBeClickable(businessUnitDropDown.$('button')));
+        await businessUnitDropDown.$('button').click();
+        await browser.wait(this.EC.visibilityOf(businessUnitDropDown.$('input')));
+        await businessUnitDropDown.$('input').sendKeys(businessUnit);
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await businessUnitDropDown.$$(this.selectors.selectOptions).count();
+            return count >= 1;
+        }))
+        await browser.wait(this.EC.elementToBeClickable(element(by.cssContainingText(this.selectors.selectOptions, businessUnit))));
+        await element(by.cssContainingText(this.selectors.selectOptions, businessUnit)).click();
+    }
+
+    async selectDepartment(department:string): Promise<void> {
+        const departmentDropdown = $$(this.selectors.assignmentDropDownList).get(2);
+        await browser.wait(this.EC.elementToBeClickable(departmentDropdown.$('button')));
+        await departmentDropdown.$('button').click();
+        await browser.wait(this.EC.visibilityOf(departmentDropdown.$('input')));
+        await departmentDropdown.$('input').sendKeys(department);
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await departmentDropdown.$$(this.selectors.selectOptions).count();
+            return count >= 1;
+        }));
+        await browser.wait(this.EC.elementToBeClickable(element(by.cssContainingText(this.selectors.selectOptions, department))));
+        await element(by.cssContainingText(this.selectors.selectOptions, department)).click();
+    }
+
+    async selectSupportGroup(supportGroup:string): Promise<void> {
+        const supportGroupDropDown = $$(this.selectors.assignmentDropDownList).get(3);
+        await browser.wait(this.EC.elementToBeClickable(supportGroupDropDown.$('button')));
+        await supportGroupDropDown.$('button').click();
+        await browser.wait(this.EC.visibilityOf(supportGroupDropDown.$('input')));
+        await supportGroupDropDown.$('input').sendKeys(supportGroup);
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await supportGroupDropDown.$$(this.selectors.selectOptions).count();
+            return count >= 1;
+        }))
+        await browser.wait(this.EC.elementToBeClickable(element(by.cssContainingText(this.selectors.selectOptions, supportGroup))));
+        await element(by.cssContainingText(this.selectors.selectOptions, supportGroup)).click();
+    }
+
+    async selectAssignee(name:string): Promise<void> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.searchAsignee)));
+        await $(this.selectors.searchAsignee).sendKeys(name);
+        browser.actions().sendKeys(protractor.Key.ENTER).perform();
+        await browser.wait(this.EC.or(async ()=>{
+            let count = await $$(this.selectors.assignee).count();
+            return count >= 2;
+        }))
+        await element(by.cssContainingText(this.selectors.assignee, name)).click();
+    }
+}
+
+export default new changeAssignmentBlade();
