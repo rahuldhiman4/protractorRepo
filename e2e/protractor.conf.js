131b58b564299dd3b673ef047cc6c8a77649bc3a
Garud, Tushar <tushar_garud@bmc.com>
2019-11-27 19:01:19 +0530

    reporting (#48)

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,6 +3,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+var HtmlReporter = require('protractor-beautiful-reporter');
 
 /**
  * @type { import("protractor").Config }
@@ -10,7 +11,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 50000,
   specs: [
-    './tests/**/case-sla-progress.e2e-spec.ts'
+    './tests/**/*.e2e-spec.ts'
   ],
   capabilities: {
     browserName: 'chrome'
@@ -21,12 +22,16 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 60000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'reports/screenshots'
+    }).getJasmine2Reporter());
   }
 };
