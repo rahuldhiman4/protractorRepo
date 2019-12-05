9e69d45efadfe35c2658b17a3181c91b129cac39
Tidke, Pravin <pravin_tidke_tp@bmc.com>
2019-12-05 18:54:43 +0530

    7 test cases case template (#85)

--- e2e/tests/case/casetemplate.e2e-spec.ts
+++ e2e/tests/case/casetemplate.e2e-spec.ts
@@ -1,12 +1,9 @@
 import { browser } from "protractor";
 import loginPage from "../../pageobject/login.po";
 import createCaseTemplate from "../../pageobject/case/create-casetemplate.po";
-import editCaseTemplate from "../../pageobject/case/edit-casetemplate.po";
 import viewCaseTemplate from "../../pageobject/case/view-casetemplate.po";
 import navigationPage from "../../pageobject/navigation.po";
 import consoleCasetemplatePo from '../../pageobject/case/console-casetemplate.po';
-import apiHelper from '../../api/api.helper';
-import editCasetemplatePo from '../../pageobject/case/edit-casetemplate.po';
 import utilCommon from '../../utils/ui/util.common';
 
 describe('Copy Case Template', () => {
@@ -21,26 +18,86 @@ describe('Copy Case Template', () => {
 
     afterAll(async () => {
         await navigationPage.signOut();
-    }); 
+    });
+
+    it('DRDMV-10477 ,DRDMV-10483 : Case Template creation with Template validation as OPTIONAL using BA login', async () => {
+        await navigationPage.gotoSettingsPage();
+        expect(await navigationPage.gotoSettingsMenuItem('Case Management--Templates', 'Case Templates - Business Workflows')).toEqual('Case Templates - Business Workflows');
+        var caseTemplate = require('../../data/ui/casetemplate.ui.json');
+        var caseTemplateName: string = await caseTemplate['caseTemplateWitAllFields'].templateName + Math.floor(Math.random() * 100000);
+        caseTemplate['caseTemplateWitAllFields'].templateName = caseTemplateName;
+        await consoleCasetemplatePo.clickOnCreateCaseTemplateButton();
+        await createCaseTemplate.setTemplateName(caseTemplateName);
+        await createCaseTemplate.setCompanyName(caseTemplate['caseTemplateWitAllFields'].company);
+        await createCaseTemplate.setCaseSummary(caseTemplate['caseTemplateWitAllFields'].templateSummary);
+        await createCaseTemplate.setPriorityValue(caseTemplate['caseTemplateWitAllFields'].casePriority);
+        await createCaseTemplate.setOwnerGroupDropdownValue(caseTemplate['caseTemplateWitAllFields'].ownerGroup);
+        await createCaseTemplate.setTemplateStatusDropdownValue(caseTemplate['caseTemplateWitAllFields'].templateStatus)
+        await createCaseTemplate.setIdentityValidationValue(caseTemplate['caseTemplateWitAllFields'].identityValidation)
+        await createCaseTemplate.clickSaveCaseTemplate();
+        await utilCommon.waitUntilPopUpDisappear();
+        expect(await viewCaseTemplate.getCaseTemplateNameValue()).toContain(caseTemplateName);
+        expect(await viewCaseTemplate.getIdentityValdationValue()).toContain(caseTemplate['caseTemplateWitAllFields'].identityValidation);
+    });
+
+    it('DRDMV-10469 : Case Template creation with Template validation as ENFORCED', async () => {
+        await navigationPage.gotoSettingsPage();
+        expect(await navigationPage.gotoSettingsMenuItem('Case Management--Templates', 'Case Templates - Business Workflows')).toEqual('Case Templates - Business Workflows');
+        var caseTemplate = require('../../data/ui/casetemplate.ui.json');
+        var caseTemplateName: string = await caseTemplate['caseTemplateWitAllFields'].templateName + Math.floor(Math.random() * 100000);
+        caseTemplate['caseTemplateWitAllFields'].templateName = caseTemplateName;
+        await consoleCasetemplatePo.clickOnCreateCaseTemplateButton();
+        await createCaseTemplate.setTemplateName(caseTemplateName);
+        await createCaseTemplate.setCompanyName(caseTemplate['caseTemplateWitAllFields'].company);
+        await createCaseTemplate.setCaseSummary(caseTemplate['caseTemplateWitAllFields'].templateSummary);
+        await createCaseTemplate.setPriorityValue(caseTemplate['caseTemplateWitAllFields'].casePriority);
+        await createCaseTemplate.setOwnerGroupDropdownValue(caseTemplate['caseTemplateWitAllFields'].ownerGroup);
+        await createCaseTemplate.setTemplateStatusDropdownValue(caseTemplate['caseTemplateWitAllFields'].templateStatus)
+        await createCaseTemplate.setIdentityValidationValue('Enforced')
+        await createCaseTemplate.clickSaveCaseTemplate();
+        await utilCommon.waitUntilPopUpDisappear();
+        expect(await viewCaseTemplate.getCaseTemplateNameValue()).toContain(caseTemplateName);
+        expect(await viewCaseTemplate.getIdentityValdationValue()).toContain('Enforced');
+    });
+
+    it('DRDMV-10481 : Case Template creation with Template validation as NONE', async () => {
+        await navigationPage.gotoSettingsPage();
+        expect(await navigationPage.gotoSettingsMenuItem('Case Management--Templates', 'Case Templates - Business Workflows')).toEqual('Case Templates - Business Workflows');
+        var caseTemplate = require('../../data/ui/casetemplate.ui.json');
+        var caseTemplateName: string = await caseTemplate['caseTemplateWitAllFields'].templateName + Math.floor(Math.random() * 100000);
+        caseTemplate['caseTemplateWitAllFields'].templateName = caseTemplateName;
+        await consoleCasetemplatePo.clickOnCreateCaseTemplateButton();
+        await createCaseTemplate.setTemplateName(caseTemplateName);
+        await createCaseTemplate.setCompanyName(caseTemplate['caseTemplateWitAllFields'].company);
+        await createCaseTemplate.setCaseSummary(caseTemplate['caseTemplateWitAllFields'].templateSummary);
+        await createCaseTemplate.setPriorityValue(caseTemplate['caseTemplateWitAllFields'].casePriority);
+        await createCaseTemplate.setOwnerGroupDropdownValue(caseTemplate['caseTemplateWitAllFields'].ownerGroup);
+        await createCaseTemplate.setTemplateStatusDropdownValue(caseTemplate['caseTemplateWitAllFields'].templateStatus)
+        await createCaseTemplate.setIdentityValidationValue('None')
+        await createCaseTemplate.clickSaveCaseTemplate();
+        await utilCommon.waitUntilPopUpDisappear();
+        expect(await viewCaseTemplate.getCaseTemplateNameValue()).toContain(caseTemplateName);
+        expect(await viewCaseTemplate.getIdentityValdationValue()).toContain('None');
+    });
 
     it('DRDMV-10477 : Case Template creation with Template validation as OPTIONAL using BA login', async () => {
-            await navigationPage.gotoSettingsPage();
-            expect(await navigationPage.gotoSettingsMenuItem('Case Management--Templates', 'Case Templates - Business Workflows')).toEqual('Case Templates - Business Workflows');
-            var caseTemplate = require('../../data/ui/casetemplate.ui.json');
-            var caseTemplateName: string = await caseTemplate['caseTemplateWitAllFields'].templateName + Math.floor(Math.random() * 100000);
-            caseTemplate['caseTemplateWitAllFields'].templateName = caseTemplateName;
-            await consoleCasetemplatePo.clickOnCreateCaseTemplateButton();
-            await createCaseTemplate.setTemplateName(caseTemplateName);
-            await createCaseTemplate.setCompanyName(caseTemplate['caseTemplateWitAllFields'].company);
-            await createCaseTemplate.setCaseSummary(caseTemplate['caseTemplateWitAllFields'].templateSummary);
-            await createCaseTemplate.setPriorityValue(caseTemplate['caseTemplateWitAllFields'].casePriority);
-            await createCaseTemplate.setOwnerGroupDropdownValue(caseTemplate['caseTemplateWitAllFields'].ownerGroup);
-            await createCaseTemplate.setTemplateStatusDropdownValue(caseTemplate['caseTemplateWitAllFields'].templateStatus)
-            await createCaseTemplate.setIdentityValidationValue(caseTemplate['caseTemplateWitAllFields'].identityValidation)
-            await createCaseTemplate.clickSaveCaseTemplate();
-            await utilCommon.waitUntilPopUpDisappear();
-            expect(await viewCaseTemplate.getCaseTemplateNameValue()).toContain(caseTemplateName);
-            expect(await viewCaseTemplate.getIdentityValdationValue()).toContain(caseTemplate['caseTemplateWitAllFields'].identityValidation);
+        await navigationPage.gotoSettingsPage();
+        expect(await navigationPage.gotoSettingsMenuItem('Case Management--Templates', 'Case Templates - Business Workflows')).toEqual('Case Templates - Business Workflows');
+        var caseTemplate = require('../../data/ui/casetemplate.ui.json');
+        var caseTemplateName: string = await caseTemplate['caseTemplateWitAllFields'].templateName + Math.floor(Math.random() * 100000);
+        caseTemplate['caseTemplateWitAllFields'].templateName = caseTemplateName;
+        await consoleCasetemplatePo.clickOnCreateCaseTemplateButton();
+        await createCaseTemplate.setTemplateName(caseTemplateName);
+        await createCaseTemplate.setCompanyName(caseTemplate['caseTemplateWitAllFields'].company);
+        await createCaseTemplate.setCaseSummary(caseTemplate['caseTemplateWitAllFields'].templateSummary);
+        await createCaseTemplate.setPriorityValue(caseTemplate['caseTemplateWitAllFields'].casePriority);
+        await createCaseTemplate.setOwnerGroupDropdownValue(caseTemplate['caseTemplateWitAllFields'].ownerGroup);
+        await createCaseTemplate.setTemplateStatusDropdownValue(caseTemplate['caseTemplateWitAllFields'].templateStatus)
+        await createCaseTemplate.setIdentityValidationValue(caseTemplate['caseTemplateWitAllFields'].identityValidation)
+        await createCaseTemplate.clickSaveCaseTemplate();
+        await utilCommon.waitUntilPopUpDisappear();
+        expect(await viewCaseTemplate.getCaseTemplateNameValue()).toContain(caseTemplateName);
+        expect(await viewCaseTemplate.getIdentityValdationValue()).toContain(caseTemplate['caseTemplateWitAllFields'].identityValidation);
     });
 
     it('DRDMV-10476 : Case Template creation with Template validation as OPTIONAL using tadmin login', async () => {
@@ -73,7 +130,7 @@ describe('Copy Case Template', () => {
         }
     });
 
-    it('DRDMV-10476 : Case Template creation with Template validation as OPTIONAL using tadmin login', async () => {
+    it('DRDMV-10479 : Case Template NOT created with Template validation as OPTIONAL using Case Agent login', async () => {
         try {
             await navigationPage.signOut();
             await loginPage.login('franz');
@@ -85,5 +142,7 @@ describe('Copy Case Template', () => {
         } finally {
             await navigationPage.signOut();
             await loginPage.login('qkatawazi');
-        }});
+        }
+    });
+
 });
