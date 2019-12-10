8b089dd2942835478fe8adb17a38f3aaca203303
radhiman <rahul_dhiman_tp@bmc.com>
2019-12-10 18:02:13 +0530

    DRDMV-17442 - review comments

--- e2e/tests/knowledge/create-knowledge-article.e2e-spec.ts
+++ e2e/tests/knowledge/create-knowledge-article.e2e-spec.ts
@@ -55,7 +55,7 @@ describe('Knowledge Article', () => {
         let knowledgeDataFile = require("../../data/ui/knowledge/knowledgeArticle.ui.json")
         let knowledgeData = knowledgeDataFile['DRDMV-19020'];
         await navigationPage.signOut();
-        await loginPage.login(personData.userId);
+        await loginPage.loginWithCredentials(personData.userId + "@petramco.com", 'Password_1234');
         await navigationPage.gotoCreateKnowledge();
         await createKnowledgePage.clickOnTemplate(knowledgeData.TemplateName);
         await createKnowledgePage.clickOnUseSelectedTemplateButton();
