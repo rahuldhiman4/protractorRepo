f035179a8f61c51f771feb183b3239a52e46766c
Gaikwad, Kiran <kiran_gaikwad_tp@bmc.com>
2019-12-12 18:24:57 +0530

    DRDMV-17650 (#109)

--- e2e/pageobject/case/case-console.po.ts
+++ e2e/pageobject/case/case-console.po.ts
@@ -1,8 +1,11 @@
 import { $, $$, browser, Key, protractor, ProtractorExpectedConditions } from "protractor";
+import utilCommon from '../../utils/util.common';
+import gridUtil from '../../utils/util.grid';
 
 class CaseConsolePage {
     EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
     selectors = {
+        guid: "d628a20f-e852-4a84-87e6-f5191f77ddf6",
         searchCase: '[rx-id="search-text-input"]',
         recommendedCaseLink: '.ui-grid__link',
         recommendedCaseCheckBox: '.ui-grid-icon-ok',
@@ -11,6 +14,34 @@ class CaseConsolePage {
         applyFilter: '.rx-search-filter-heading__apply',
         removeFilter: '..d-tag-remove-button',
         tableValue: '.ui-grid-cell-contents',
+    }
+
+    async searchCase(caseId: string): Promise<void> {
+        await gridUtil.clearFilter();
+        await gridUtil.searchRecord(caseId);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async searchAndOpenCase(caseId: string): Promise<void> {
+        await gridUtil.clearFilter();
+        await gridUtil.searchAndOpenHyperlink(caseId);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async isCaseIdPresent(caseId: string): Promise<boolean> {
+        let priorityValue = await gridUtil.getSelectedGridRecordValue(this.selectors.guid, 'Case ID');
+        return caseId === priorityValue;
+    }
+
+    async isCasePriorityPresent(priority: string): Promise<boolean> {
+        let priorityValue = await gridUtil.getSelectedGridRecordValue(this.selectors.guid, 'Priority');
+        return priority === priorityValue;
+    }
+
+    async isCaseStatusPresent(status: string): Promise<boolean> {
+        let priorityValue = await gridUtil.getSelectedGridRecordValue(this.selectors.guid, 'Status');
+        return status === priorityValue;
+
     }
 
     async setCaseSearchBoxValue(input: string): Promise<void> {
@@ -33,6 +64,12 @@ class CaseConsolePage {
         await browser.wait(this.EC.elementToBeClickable($(this.selectors.recommendedCaseCheckBox)));
         await $(this.selectors.recommendedCaseCheckBox).click();
     }
+
+    async isCaseSummaryPresent(summary: string): Promise<boolean> {
+        let priorityValue = await gridUtil.getSelectedGridRecordValue(this.selectors.guid, 'Summary');
+        return summary === priorityValue;
+    }
+
 }
 
 export default new CaseConsolePage();
---
+++ e2e/tests/ticketing/menu-item.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { browser } from "protractor";
+import loginPage from "../../pageobject/common/login.po";
+import navigationPage from "../../pageobject/common/navigation.po";
+import createMenuItems from '../../pageobject/settings/application-config/create-menu-items-blade.po';
+import utilCommon from '../../utils/util.common';
+
+describe('Menu Item', () => {
+    beforeAll(async () => {
+        await browser.get('/innovationsuite/index.html#/com.bmc.dsm.bwfa');
+        await loginPage.login('qkatawazi');
+    });
+
+    afterAll(async () => {
+        await navigationPage.signOut();
+    });
+
+    afterEach(async () => {
+        await browser.refresh();
+        await utilCommon.waitUntilSpinnerToHide();
+    });
+
+    //kgaikwad
+    it('DRDMV-17650: The Menu Items View would be re-arranged so that fields are in Proper sequence.', async () => {
+        await navigationPage.gotoSettingsPage();
+        await navigationPage.gotoSettingsMenuItem('Application Configuration--Menu Items', 'Menu Items - Business Workflows');
+        await createMenuItems.clickOnMenuOptionLink();
+        await createMenuItems.selectMenuNameDropDown('Label');
+        await createMenuItems.selectMenuNameDropDown('Resolution Code');
+        await createMenuItems.selectMenuNameDropDown('Source');
+        expect(await createMenuItems.isMenuNameDropDownPresent()).toBeTruthy('MenuName Drop down is missing');
+        expect(await createMenuItems.isMenuOptionTextBoxPresent()).toBeTruthy('MenuOption text box is missing');
+        expect(await createMenuItems.isStatusDropDownPresent()).toBeTruthy('status Drop down is missing');
+        await createMenuItems.selectStatusDropDown('Active');
+        await createMenuItems.selectStatusDropDown('Inactive');
+        await createMenuItems.selectStatusDropDown('Deprecated');
+        expect(await createMenuItems.isToggleButtonPresent()).toBeTruthy('Toggle Button is missing');
+    });
+
+    //kgaikwad
+    it('DRDMV-17637: [UI] "Resolution Code" new option available in Menu Items', async () => {
+        await navigationPage.gotoSettingsPage();
+        await navigationPage.gotoSettingsMenuItem('Application Configuration--Menu Items', 'Menu Items - Business Workflows');
+        await createMenuItems.clickOnMenuOptionLink();
+        await createMenuItems.selectMenuNameDropDown('Resolution Code');
+    });
+
+})
