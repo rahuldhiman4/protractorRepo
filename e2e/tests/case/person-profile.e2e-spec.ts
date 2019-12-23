af772683e448a87988fd704501b58ce4a892bdee
asahitya <anant_sahitya@bmc.com>
2019-12-23 18:37:35 +0530

    Self review

--- e2e/tests/case/person-profile.e2e-spec.ts
+++ e2e/tests/case/person-profile.e2e-spec.ts
@@ -22,18 +22,22 @@ describe('Person Profile test', () => {
         await utilCommon.waitUntilSpinnerToHide();
     });
 
+    //asahitya
     it('DRDMV-14085: Verify Profile picture of logged in user on My profile page', async () => {
         expect(await personProfile.isPersonProfileImageDisplayed()).toBeTruthy("Person Profile image is not displayed");
     });
 
+    //asahitya
     it('DRDMV-14086: Verify Profile picture of Managar-Logged in user on My Profile page', async () => {
         expect(await personProfile.isPersonManagerImageDisplayed()).toBeTruthy("Person Manager image is not displayed");
     });
 
+    //asahitya
     it('DRDMV-17018: Check agent can not add notes to own Person profile in agent work history tab', async () => {
         expect(await personProfile.isActivityNotesDisplayed()).toBeFalsy("Activity Notes are available");
     });
 
+    //asahitya
     it('DRDMV-14087: Verify cases visible in Requested cases tab of My profile page are according to permissions of logged in user', async () => {
         await personProfile.navigateToTab("Requested Cases");
         await apiHelper.apiLogin("qtao");
@@ -43,6 +47,7 @@ describe('Person Profile test', () => {
         expect(await personProfile.isCasePresentOnRequestedCases(caseDisplayId)).toBeTruthy("Case is not present");
     });
 
+    //asahitya
     it('DRDMV-14088: Verify cases visible in Assiged cases tab of My profile page are according to permissions of logged in user', async () => {
         await apiHelper.apiLogin("qtao");
         let caseData = require('../../data/ui/case/case.ui.json');
@@ -52,6 +57,7 @@ describe('Person Profile test', () => {
         expect(await personProfile.isCasePresentOnAssignedCases(caseDisplayId)).toBeTruthy("Case is not present");
     });
 
+    //asahitya
     it('DRDMV-14023: Verify My Profile Console', async () => {
         expect(await personProfile.getCompany()).toContain("Petramco", "Company name mismatch");
         expect(await personProfile.getContactNumber()).toBe("+19255553456", "Phone number mismatch");
@@ -65,6 +71,7 @@ describe('Person Profile test', () => {
         await personProfile.navigateToTab("Related Persons");
     });
 
+    //asahitya
     it('DRDMV-14025: Verify navigation to Managers Profile from My Profile->Assigned Manager', async () => {
         await personProfile.clickOnManagerLink();
         expect(await personProfile.getCompany()).toContain("Petramco", "Company name mismatch");
@@ -78,12 +85,14 @@ describe('Person Profile test', () => {
         await personProfile.navigateToTab("Related Persons");
     });
 
+    //asahitya
     it('DRDMV-17020: Check agent can view the notes of other agents Person profile in agent work history tab for which he is submitter of the note', async () => {
         await activityTabPage.addActivityNote("DRDMV-17020");
         await activityTabPage.clickOnPostButton();
         expect(await activityTabPage.isTextPresentInActivityLog("DRDMV-17020")).toBeTruthy("Activity notes is missing");
     });
 
+    //asahitya
     it('DRDMV-17019: Check agent cannot view notes to own Person profile in agent work history tab', async () => {
         await activityTabPage.addActivityNote("DRDMV-17019");
         await activityTabPage.clickOnPostButton();
@@ -103,6 +112,7 @@ describe('Person Profile test', () => {
         }
     });
 
+    //asahitya
     it('DRDMV-14028: Verify Requested Cases tab of My Profile console', async () => {
         await navigationPage.goToPersonProfile();
         await personProfile.navigateToTab("Requested Cases");
@@ -130,6 +140,7 @@ describe('Person Profile test', () => {
         expect(await personProfile.isRequestedCasesColumnsSortedAscending("Case ID")).toBeTruthy("Columns are not sorted");
     });
 
+    //asahitya
     it('DRDMV-14029: Verify Assigned Cases tab of My Profile console', async () => {
         await navigationPage.goToPersonProfile();
         await personProfile.navigateToTab("Assigned Cases");
@@ -156,7 +167,5 @@ describe('Person Profile test', () => {
         //Verify sorting
         expect(await personProfile.isAssignedCasesColumnsSortedAscending("Case ID")).toBeTruthy("Columns are not sorted");
     });
-
-
 
 })
