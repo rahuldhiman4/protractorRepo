1c229605ff8fc19c732bd80f39085e518edcbbac
ptidke <pravin_tidke_tp@bmc.com>
2019-12-24 14:47:43 +0530

    review

--- e2e/api/api.helper.ts
+++ e2e/api/api.helper.ts
@@ -6,13 +6,12 @@ import { ISupportGroup } from 'e2e/data/api/interface/support.group.interface.ap
 import { ITaskTemplate } from 'e2e/data/api/interface/task.template.interface.api';
 import { browser } from 'protractor';
 import { default as apiCoreUtil, default as coreApi } from "../api/api.core.util";
-import { CaseTemplate, TaskTemplate, MenuItemStatus } from "../api/constant.api";
+import { CaseTemplate, MenuItemStatus, TaskTemplate } from "../api/constant.api";
 import { ICaseTemplate } from "../data/api/interface/case.template.interface.api";
-import { IFlowset } from '../data/api/interface/flowset.interface.api';
-import { INotesTemplate } from '../data/api/interface/notes.template.interface.api';
 import { IDomainTag } from '../data/api/interface/domain.tag.interface.api';
-import { IMenuItem } from '../data/api/interface/menu.Items.interface.api';
-import { IEmailTemplate } from '../data/api/interface/email.template.interface.api';
+import { IFlowset } from '../data/api/interface/flowset.interface.api';
+import { IMenuItem } from '../data/api/interface/menu.items.interface.api';
+import { INotesTemplate } from '../data/api/interface/notes.template.interface.api';
 
 axios.defaults.baseURL = browser.baseUrl;
 axios.defaults.headers.common['X-Requested-By'] = 'XMLHttpRequest';
@@ -167,6 +166,7 @@ class ApiHelper {
 
         var templateDataFile = await require('../data/api/task/task.template.api.json');
         var templateData = await templateDataFile.AutoTaskTemplateNewProcess;
+
         templateData.fieldInstances[7].value = TaskTemplate[data.templateStatus];
         templateData.fieldInstances[8].value = data.templateSummary;
         templateData.fieldInstances[1000001437].value = data.templateName;
@@ -436,23 +436,6 @@ class ApiHelper {
         apiCoreUtil.updateRecordInstance("com.bmc.dsm.case-lib:Case Template", caseTemplateGuid, caseTemplateJsonData);
     }
 
-    async createEmailTemplate(data: IEmailTemplate): Promise<boolean> {
-        let emailTemplateFile = await require('../data/api/email/email.template.api.json');
-        let templateData = await emailTemplateFile.EmailTemplateData;
-        let companyGuid = await coreApi.getOrganizationGuid(data.Company);
-        templateData.processInputValues["Company"] = companyGuid;
-        templateData.processInputValues["TemplateName"] = data.TemplateName;
-        templateData.processInputValues["Status"] = data.Status;
-        templateData.processInputValues["Description"] = data.Description;
-        templateData.processInputValues["EmailMessageSubject"] = data.EmailMessageSubject;
-        templateData.processInputValues["EmailMessageBody"] = data.EmailMessageBody;
-        templateData.processInputValues["Module"] = "Cases";
-        templateData.processInputValues["Source Definition Name"] = "com.bmc.dsm.case-lib:Case";
-        const newTemplate = await coreApi.createEmailOrNotesTemplate(templateData);
-        console.log('Create Email Template API Status =============>', newTemplate.status);
-        return newTemplate.status == 201;
-    }
-
     async createNotesTemplate(module: string, data: INotesTemplate): Promise<boolean> {
         let notesTemplateFile = await require('../data/api/social/notes.template.api.json');
         let templateData = await notesTemplateFile.NotesTemplateData;
@@ -492,7 +475,7 @@ class ApiHelper {
                 break;
             }
         }
-        const newTemplate = await coreApi.createEmailOrNotesTemplate(templateData);
+        const newTemplate = await coreApi.createNotesTemplate(templateData);
         console.log('Create Notes Template API Status =============>', newTemplate.status);
         return newTemplate.status == 201;
     }
@@ -517,22 +500,22 @@ class ApiHelper {
         };
     }
 
-    async createNewMenuItem(data: IMenuItem): Promise<IIDs> {
+    async createNewMenuItem(data: IMenuItem): Promise<IIDs> {
         let randomStr = [...Array(6)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
         console.log(data);
         let menuItemFile = await require('../data/api/shared-services/menuItemConfiguration.api.json');
-        console.log('New Menu Item API Status =============>');
+        console.log('New Menu Item API Status =============>');
         let menuItemData = await menuItemFile.MenuItemConfiguration;
         menuItemData.fieldInstances[450000153].value = data.menuType;
         menuItemData.fieldInstances[450000152].value = data.menuItemName;
         menuItemData.fieldInstances[7].value = MenuItemStatus[data.menuItemStatus];
         menuItemData.fieldInstances[450000154].value = randomStr;
-        console.log('New Menu Item Data =============>' + menuItemData);
+        console.log('New Menu Item Data =============>' + menuItemData);
         const menuItem = await coreApi.createRecordInstance(menuItemData);
         const menuItemDetails = await axios.get(
             menuItem.headers.location
         );
-        console.log('New Menu Item API Status =============>', menuItemDetails.status);
+        console.log('New Menu Item API Status =============>', menuItemDetails.status);
 
         return {
             id: menuItemDetails.data.id,
