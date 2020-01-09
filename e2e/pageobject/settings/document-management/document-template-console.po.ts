eab1776efd9df481ddc62d964327201a7d9f10c9
Gaikwad <kgaikwad@bmc.com>
2020-01-09 17:38:14 +0530

    DRDMV-14970,DRDMV-14974,DRDMV-14971,DRDMV-14972

---
+++ e2e/pageobject/settings/document-management/document-template-console.po.ts
@@ -0,0 +1,80 @@
+import { element, by, $, protractor, ProtractorExpectedConditions, browser } from "protractor";
+import utilCommon from '../../../utils/util.common';
+import utilGrid from '../../../utils/util.grid';
+
+class DocumentTemplateConsolePage {
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors = {
+            gridGuid: '9a22b025-c87e-4ddb-b8ce-472ccbf39a63',
+            pageHeader: '.rx-theme-main-text-color h2',
+            refreshButton: '.d-icon-refresh',
+            deleteButton: '[rx-view-component-id="93f7643a-ab88-41be-9ff4-7a9130dbbff0"] .d-button_small span',
+    }
+
+    async selectCheckBox(record:string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.deleteButton)));
+        await utilGrid.searchAndSelectFirstCheckBoxWOGrid(record);
+    }
+
+    async clickOnDeleteButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.deleteButton)));
+        await $(this.selectors.deleteButton).click();
+    }
+
+    async clickOnGridRefreshButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.refreshButton)));
+        await $(this.selectors.refreshButton).click();
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async isHeaderDisplayed(headerName:string): Promise<boolean> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.pageHeader)));
+        return await element(by.cssContainingText(this.selectors.pageHeader,headerName)).isDisplayed();
+    }
+
+    async isGridRecordPresent(searchRecord:string): Promise<boolean> {
+        await utilGrid.searchOnGridConsole(searchRecord);
+        return await utilGrid.isGridRecordPresent();
+    }
+
+    async searchOnGridConsole(value: string): Promise<void> {
+        await utilGrid.searchOnGridConsole(value);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async addColumnOnGrid(columnHeader: string[]): Promise<void> {
+        await utilGrid.addGridColumn(this.selectors.gridGuid, columnHeader);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async clearGridSearchBox(): Promise<void> {
+        await utilGrid.clearGridSearchBox();
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async removeColumnOnGrid(columnHeader: string[]): Promise<void> {
+        await utilGrid.removeGridColumn(this.selectors.gridGuid, columnHeader);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async isGridColumnSorted(columnHeader: string, sortType: string): Promise<boolean> {
+        return await utilGrid.isGridColumnSorted(columnHeader, sortType, this.selectors.gridGuid);
+    }
+
+    async getSelectedGridRecordValue(columnHeader: string): Promise<string> {
+        return await utilGrid.getSelectedGridRecordValue(this.selectors.gridGuid, columnHeader);
+    }
+
+    async areGridColumnHeaderMatches(columnHeader: string[]): Promise<boolean> {
+        return await utilGrid.areColumnHeaderMatches(this.selectors.gridGuid, columnHeader);
+    }
+
+    async searchAndOpenDocumentTemplate(value: string): Promise<void> {
+        await utilGrid.searchAndOpenHyperlink(value);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+
+}
+
+export default new DocumentTemplateConsolePage();
---
+++ e2e/pageobject/settings/document-management/edit-document-template.po.ts
@@ -0,0 +1,74 @@
+import { $, browser, by, element, protractor, ProtractorExpectedConditions } from "protractor";
+import utilCommon from '../../../utils/util.common';
+
+class EditDocumentLibraryPage {
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors = {
+        templateName: '[rx-view-component-id="4b7649f7-fcca-4cd5-80f5-534f41d8a05e"] input',
+        documentBody: '[rx-view-component-id="f6baa44c-1e91-49be-9164-1c56077900d8"] div.cke_wysiwyg_div',
+        company: '[rx-view-component-id="0b925817-7212-418b-adf7-1403351d6b0c"] .ui-select-toggle',
+        description: '[rx-view-component-id="c5ac0f65-e850-43aa-8040-00434db0acc9"] input',
+        saveButton: '[rx-view-component-id="0afac686-fde8-4877-854b-7b8da0f3a9fa"] button',
+        cancelButton: '[rx-view-component-id="55363c3b-863f-4f6c-b8c4-fe78e55208dc"] button',
+        labelDropDown: '[rx-view-component-id="72d4bfbf-ec5c-437e-b0f1-f216babb58f8"] .ui-select-allow-clear',
+        labelDropDownGuid: '72d4bfbf-ec5c-437e-b0f1-f216babb58f8',
+        pageHeader: '.modal-title',
+    }
+
+    async selectLabelDropDown(value: string): Promise<void> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.cancelButton)));
+        await utilCommon.selectDropDown(this.selectors.labelDropDownGuid, value);
+    }
+
+    async isHeaderDisplayed(headerName: string): Promise<boolean> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.pageHeader)));
+        return await element(by.cssContainingText(this.selectors.pageHeader, headerName)).isDisplayed();
+    }
+
+    async clickOnSaveButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.saveButton)));
+        await $(this.selectors.saveButton).click();
+    }
+
+    async clickOnCancelButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.cancelButton)));
+        await $(this.selectors.cancelButton).click();
+    }
+
+    async isTemplateNameDisplayed(templateName: string): Promise<boolean> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.templateName)));
+        let attribute = await $(this.selectors.templateName).getAttribute('value');
+        return attribute == templateName ? true : false
+    }
+
+    async isCompanyDropDownDisabled(): Promise<boolean> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.company)));
+        return await $(this.selectors.company).getAttribute('disabled') == 'true';
+    }
+
+    async isCompanyNameDisplayed(companyName: string): Promise<boolean> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.company)));
+        let getText = await $(this.selectors.company).$('.ui-select-match-text').getText();
+        return getText == companyName ? true : false
+    }
+
+    async isLabelValueDisplayed(companyName: string): Promise<boolean> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.labelDropDown)));
+        let getText = await $(this.selectors.labelDropDown).getText();
+        return getText == companyName ? true : false
+    }
+
+    async isDescriptionValueDisplayed(descriptionName: string): Promise<boolean> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.description)));
+        let attribute = await $(this.selectors.description).getAttribute('value');
+        return attribute == descriptionName ? true : false
+    }
+
+    async isDocumentBodyDisplayed(documentBody: string): Promise<boolean> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.documentBody)));
+        let attribute = await $(this.selectors.documentBody).getText();
+        return attribute == documentBody ? true : false
+    }
+}
+
+export default new EditDocumentLibraryPage();
