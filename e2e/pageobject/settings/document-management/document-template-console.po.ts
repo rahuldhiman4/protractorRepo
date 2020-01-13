836c9af869d78f02b44a4447b7ccd80153554521
Gaikwad, Kiran <kiran_gaikwad_tp@bmc.com>
2020-01-13 12:43:09 +0530

    DRDMV-14970,DRDMV-14974,DRDMV-14971,DRDMV-14972 (#153)

--- e2e/pageobject/settings/document-management/document-template-console.po.ts
+++ e2e/pageobject/settings/document-management/document-template-console.po.ts
@@ -1,12 +1,32 @@
-import { protractor, ProtractorExpectedConditions, browser } from "protractor";
+import { element, by, $, protractor, ProtractorExpectedConditions, browser } from "protractor";
 import utilCommon from '../../../utils/util.common';
 import utilGrid from '../../../utils/util.grid';
 
-class DocumentLibraryPage {
+class DocumentTemplateConsolePage {
     EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
     selectors = {
-        addDocumentButton: '[rx-view-component-id="c2df3218-8ef7-402c-bdc2-721e891346bb"] button',
-        gridGuid: '5d1f94a9-693e-4dbf-896f-3b9689f95a42',   
+            gridGuid: '9a22b025-c87e-4ddb-b8ce-472ccbf39a63',
+            pageHeader: '.rx-theme-main-text-color h2',
+            deleteButton: '[rx-view-component-id="93f7643a-ab88-41be-9ff4-7a9130dbbff0"] .d-button_small span',
+    }
+
+    async selectCheckBox(record:string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.deleteButton)));
+        await utilGrid.searchAndSelectFirstCheckBoxWOGrid(record);
+    }
+
+    async clickOnDeleteButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.deleteButton)));
+        await $(this.selectors.deleteButton).click();
+    }
+
+    async clickOnGridRefreshButton(): Promise<void> {
+        await utilGrid.clickOnGridRefreshButton();
+    }
+
+    async isHeaderDisplayed(headerName:string): Promise<boolean> {
+        await browser.wait(this.EC.visibilityOf($(this.selectors.pageHeader)));
+        return await element(by.cssContainingText(this.selectors.pageHeader,headerName)).isDisplayed();
     }
 
     async isGridRecordPresent(searchRecord:string): Promise<boolean> {
@@ -45,7 +65,7 @@ class DocumentLibraryPage {
         return await utilGrid.areColumnHeaderMatches(this.selectors.gridGuid, columnHeader);
     }
 
-    async searchAndOpenDocumentLibrary(value: string): Promise<void> {
+    async searchAndOpenDocumentTemplate(value: string): Promise<void> {
         await utilGrid.searchAndOpenHyperlink(value);
         await utilCommon.waitUntilSpinnerToHide();
     }
@@ -53,4 +73,4 @@ class DocumentLibraryPage {
 
 }
 
-export default new DocumentLibraryPage();
+export default new DocumentTemplateConsolePage();
