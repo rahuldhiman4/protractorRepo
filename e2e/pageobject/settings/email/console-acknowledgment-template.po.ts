31b39f72ecab4db31a91b6943741a4d9f7ed8ac4
Gaikwad, Kiran <kiran_gaikwad_tp@bmc.com>
2020-01-14 17:04:29 +0530

    kiran-DRDMV-10896,DRDMV-10901,DRDMV-10922,DRDMV-10895 (#159)

---
+++ e2e/pageobject/settings/email/console-acknowledgment-template.po.ts
@@ -0,0 +1,73 @@
+import { element, by, $, protractor, ProtractorExpectedConditions, browser } from "protractor";
+import utilCommon from '../../../utils/util.common';
+import utilGrid from '../../../utils/util.grid';
+
+class AcknowledgmentTemplateConsolePage {
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors = {
+            gridGuid: 'deb7a0eb-56a9-432d-9c4a-912cf4644086',
+            delete: '[rx-view-component-id="b21478a3-5ca4-4d00-85d3-da3a5bc4ea96"] button',
+            addAckTemplates: '.d-icon-left-plus span',
+    }
+
+    async clickOnAddAcknowlegeTemplateButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.addAckTemplates)));
+        await element(by.cssContainingText(this.selectors.addAckTemplates, 'Acknowledgment Template')).click();
+    }
+
+    async clickOnDeleteButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.delete)));
+        await $(this.selectors.delete).click();
+    }
+
+    async searchAndSelectGridRecord(value:string): Promise<void> {
+        await utilGrid.searchAndSelectGridRecord(value);
+    }
+
+    async clickOnGridRefreshButton(): Promise<void> {
+        await utilGrid.clickOnGridRefreshButton();
+    }
+
+    async isGridRecordPresent(searchRecord:string): Promise<boolean> {
+        return await utilGrid.isGridRecordPresent(searchRecord);
+    }
+
+    async searchOnGridConsole(value: string): Promise<void> {
+        await utilGrid.searchOnGridConsole(value);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async addColumnOnGrid(columnHeader: string[]): Promise<void> {
+        await utilGrid.addGridColumn(this.selectors.gridGuid, columnHeader);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async clearGridSearchBox(): Promise<void> {
+        await utilGrid.clearGridSearchBox();
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async removeColumnOnGrid(columnHeader: string[]): Promise<void> {
+        await utilGrid.removeGridColumn(this.selectors.gridGuid, columnHeader);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+
+    async isGridColumnSorted(columnHeader: string, sortType: string): Promise<boolean> {
+        return await utilGrid.isGridColumnSorted(columnHeader, sortType, this.selectors.gridGuid);
+    }
+
+    async getSelectedGridRecordValue(columnHeader: string): Promise<string> {
+        return await utilGrid.getSelectedGridRecordValue(this.selectors.gridGuid, columnHeader);
+    }
+
+    async areGridColumnHeaderMatches(columnHeader: string[]): Promise<boolean> {
+        return await utilGrid.areColumnHeaderMatches(this.selectors.gridGuid, columnHeader);
+    }
+
+    async searchAndOpenAcknowledgmentTemplate(value: string): Promise<void> {
+        await utilGrid.searchAndOpenHyperlink(value);
+        await utilCommon.waitUntilSpinnerToHide();
+    }
+}
+
+export default new AcknowledgmentTemplateConsolePage();
---
+++ e2e/pageobject/settings/email/create-acknowledgment-template.po.ts
@@ -0,0 +1,56 @@
+import { $, browser, by, element, protractor, ProtractorExpectedConditions } from "protractor";
+import utilCommon from '../../../utils/util.common';
+
+class CreateAcknowledgmentTemplateBlade {
+    EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+    selectors = {
+        templateName: '[rx-view-component-id="310bbc87-54ee-4994-9a1e-93b1982155f2"] input',
+        companyGuid: 'd240380a-1de2-4b28-9082-81e96fc21415',
+        statusGuid: '3cfbfd34-19ff-4ddb-818b-23b19c859dbe',
+        labelGuid: 'a0774e28-42c2-4132-9da4-0063545e791f',
+        description: '[rx-view-component-id="13cf801e-fc2f-4d74-a57b-77e4ebf2bde6"] input',
+        subject: '[rx-view-component-id="187510cc-9804-46e2-bbda-0cdba1d6c83c"] textarea',
+        body: '.cke_wysiwyg_div',
+        saveButton: '[rx-view-component-id="093a0eeb-c1e0-4ed8-945f-da46d9bbde88"] button',
+        cancelButton: '[rx-view-component-id="9aeef4d7-1a10-4ffd-aa3a-22665c32883c"] button',
+    }
+
+    async setTemplateName(templateName: string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.templateName)));
+        await $(this.selectors.templateName).sendKeys(templateName);
+    }
+
+    async selectCompanyDropDown(value: string): Promise<void> {
+        await utilCommon.selectDropDown(this.selectors.companyGuid, value);
+    }
+
+    async selectStatusDropDown(value: string): Promise<void> {
+        await utilCommon.selectDropDown(this.selectors.statusGuid, value);
+    }
+
+    async selectLabelDropDown(value: string): Promise<void> {
+        await utilCommon.selectDropDown(this.selectors.labelGuid, value);
+    }
+
+    async setDescription(descriptionText: string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.description)));
+        await $(this.selectors.description).sendKeys(descriptionText);
+    }
+
+    async setSubject(subject: string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.subject)));
+        await $(this.selectors.subject).sendKeys(subject);
+    }
+
+    async setBody(body: string): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.body)));
+        await $(this.selectors.body).sendKeys(body);
+    }
+
+    async clickOnSaveButton(): Promise<void> {
+        await browser.wait(this.EC.elementToBeClickable($(this.selectors.saveButton)));
+        await $(this.selectors.saveButton).click();
+    }
+}
+
+export default new CreateAcknowledgmentTemplateBlade();
