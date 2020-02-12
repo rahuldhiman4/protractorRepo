072a37c169ea695a9bbae33568f4d6762b4463aa
Gaikwad <kgaikwad@bmc.com>
2020-02-12 18:28:33 +0530

    adding protractor file

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -32,11 +32,11 @@ exports.config = {
   suites: {
     full: [
       './tests/**/*.e2e-spec.ts',
-     ]
+    ]
   },
 
   directConnect: false,
-  //  baseUrl: 'http://clm-pun-t3erts.bmc.com:8008',
+  //baseUrl: 'http://clm-pun-t3erts.bmc.com:8008',
   baseUrl: 'http://clm-aus-t5jj96.bmc.com:8008',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -48,7 +48,7 @@ exports.config = {
   async onPrepare() {
     let globals = require('protractor/built');
     let browser = globals.browser;
-    //    await browser.waitForAngularEnabled(false);
+    //await browser.waitForAngularEnabled(false);
 
     //Implicitly wait
     await browser.manage().timeouts().implicitlyWait(5000);
