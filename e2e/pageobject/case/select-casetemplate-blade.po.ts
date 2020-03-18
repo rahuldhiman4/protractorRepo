0e710237a7188c6efdfbeb293e4d81eb689775b9
Garud, Tushar <tushar_garud@bmc.com>
2020-03-17 19:47:46 -1200

    Select case template blade fix (#313)

--- e2e/pageobject/case/select-casetemplate-blade.po.ts
+++ e2e/pageobject/case/select-casetemplate-blade.po.ts
@@ -1,42 +1,39 @@
-import { $,$$, browser, Key, protractor, ProtractorExpectedConditions } from "protractor";
-import createCasePage from '../../pageobject/case/create-case.po';
+import { $, by, element, protractor, ProtractorExpectedConditions } from "protractor";
 import utilGrid from '../../utils/util.grid';
-import utilCommon from '../../utils/util.common';
-
 
 class SelectCaseTemplateBlade {
     EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
     selectors = {
-        recommendedTemplate: '[rx-view-component-id="7f305d78-5a25-4a37-be39-86d6abf6661a"] [title="Recommended Templates"]',
-        allTemplates: '[rx-view-component-id="7f305d78-5a25-4a37-be39-86d6abf6661a"] [title="All Templates"]',
+        recommendedTemplate: 'Recommended Templates',
+        allTemplates: 'All Templates',
         searchTextbox: '[rx-id="search-text-input"]',
         refreshbutton: '[rx-id="refresh-button"]',
         checkBox: 'ui-grid-icon-ok',
-        applyButton: '[rx-view-component-id="f348e681-ac02-452c-b37f-009ac4434053"] button',
+        applyButton: '[rx-view-component-id="39321025-7d92-4284-8498-a0c6fc44f6cd"] button',
         caseTemplateCheckBox: '.ui-grid-icon-ok',
         caseTemplateLink: '.ui-grid__link',
-        cancelButton: '[rx-view-component-id="161ed2e2-ea43-4db5-9f9c-149f82a74db2"] button',
+        cancelButton: '[rx-view-component-id="befd164d-2508-4b8e-9445-3ace011022b7"] button',
     }
 
     async clickOnRecommendedTemplateTab(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.recommendedTemplate)));
-        await $(this.selectors.recommendedTemplate).click();
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.recommendedTemplate)));
+        await element(by.linkText(this.selectors.recommendedTemplate)).click();
     }
 
     async clickOnCaseTemplateCheckbox(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.caseTemplateCheckBox)));
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.caseTemplateCheckBox)));
         await $(this.selectors.caseTemplateCheckBox).click();
     }
 
     async clickOnCaseTemplateLink(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.caseTemplateLink)));
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.caseTemplateLink)));
         await $(this.selectors.caseTemplateLink).click();
     }
 
     async clickOnAllTemplateTab(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.allTemplates)));
-//        await browser.wait(this.EC.visibilityOf($(this.selectors.allTemplates)));
-        await $(this.selectors.allTemplates).click();
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.allTemplates)));
+        //        await browser.wait(this.EC.visibilityOf($(this.selectors.allTemplates)));
+        await element(by.linkText(this.selectors.allTemplates)).click();
     }
 
     async searchAndOpenCaseTemplate(input: string): Promise<void> {
@@ -45,7 +42,7 @@ class SelectCaseTemplateBlade {
     }
 
     async clickOnRefreshButton(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.refreshbutton)));
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.refreshbutton)));
         await $(this.selectors.refreshbutton).click();
     }
 
@@ -53,16 +50,16 @@ class SelectCaseTemplateBlade {
         await this.clickOnAllTemplateTab();
         await utilGrid.searchAndSelectGridRecord(templateName);
         await this.clickOnApplyButton();
-//        await browser.wait(this.EC.invisibilityOf($('.modal-content')));
+        //        await browser.wait(this.EC.invisibilityOf($('.modal-content')));
     }
 
     async clickOnApplyButton(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.applyButton)));
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.applyButton)));
         await $(this.selectors.applyButton).click();
     }
 
     async clickOnCancelButton(): Promise<void> {
-//        await browser.wait(this.EC.elementToBeClickable($(this.selectors.cancelButton)));
+        //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.cancelButton)));
         await $(this.selectors.cancelButton).click();
     }
 }
