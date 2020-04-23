3336499429234e81140dbc3412822a1b521acb5e
radhiman <rahul_dhiman_tp@bmc.com>
2020-04-23 19:47:53 +0530

    Few methods fix Compose Email Class

--- e2e/pageobject/case/view-case.po.ts
+++ e2e/pageobject/case/view-case.po.ts
@@ -68,7 +68,7 @@ class ViewCasePage {
     async clickOnEmailLink(): Promise<void> {
         //        await browser.wait(this.EC.elementToBeClickable($(this.selectors.emailLink)));
         await $(this.selectors.emailLink).click();
-        await browser.wait(this.EC.visibilityOf($('.modal-content')), 2000); // wait until compose email appears (api is not involved hence EC wait)
+        //await browser.wait(this.EC.visibilityOf($('.modal-content')), 2000); // wait until compose email appears (api is not involved hence EC wait)
     }
 
     async clickOnRequestersEmail(): Promise<void> {
