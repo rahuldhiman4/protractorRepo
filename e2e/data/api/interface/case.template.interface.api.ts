f29365ab904241b064f4e569bdcc344e02b3f802
Tushar G <tushar_garud@bmc.com>
2020-05-15 19:06:34 +0530

    create case template api fix

--- e2e/data/api/interface/case.template.interface.api.ts
+++ e2e/data/api/interface/case.template.interface.api.ts
@@ -2,26 +2,27 @@ export interface ICaseTemplate {
     templateName: string;
     templateSummary: string;
     templateStatus: string;
-    company?: string;
-    ownerCompany?: string;
-    ownerBU?: string;
-    ownerGroup?: string;
+    caseCompany?: string;
     flowset?: string;
-    categoryTier1?: string;
-    categoryTier2?: string;
-    categoryTier3?: string;
-    businessUnit?: string;
-    supportGroup?: string;
-    assignee?: string;
-    description?: string;
-    resolutionCode?: boolean;
-    resolutionDescription?: boolean,
     caseStatus?: string,
     statusReason?: string,
     casePriority?: string,
+    caseDescription?: string;
+    categoryTier1?: string;
+    categoryTier2?: string;
+    categoryTier3?: string;
+    assigneeCompany?: string;
+    assigneeBU?: string;
+    assigneeSupportGroup?: string;
+    assignee?: string;
+    resolutionCode?: boolean;
+    resolutionDescription?: boolean,
+    resolveCaseonLastTaskCompletion?:string
     identityValidation?: string,
     assignmentMethod?: string,
     taskFailureConfiguration?: string,
     allowCaseReopen?: string,
-    resolveCaseonLastTaskCompletion?:string
+    ownerCompany?: string;
+    ownerBU?: string;
+    ownerGroup?: string;
 }
