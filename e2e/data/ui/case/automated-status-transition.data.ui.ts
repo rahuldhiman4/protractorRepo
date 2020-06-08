d13c3ff918cb368c41704db229a3d76a90be8676
Tushar G <tushar_garud@bmc.com>
2020-06-08 16:37:15 +0530

    automated case status fix, escape method fix

---
+++ e2e/data/ui/case/automated-status-transition.data.ui.ts
@@ -0,0 +1,7 @@
+export const AUTO_STATUS_TRANSITION_MANDATORY_FIELDS = {
+    "name": "Config Name",
+    "company": "Petramco",
+    "fromStatus": "Assigned",
+    "toStatus": "In Progress",
+    "changeStatusAfter": 2
+};
