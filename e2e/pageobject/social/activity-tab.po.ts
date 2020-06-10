91723f0543f9c7c888b34875717d905091dba400
Sahitya, Anant <anant_sahitya@bmc.com>
2020-06-10 15:13:29 +0530

    Drop 8 changes (#653)

--- e2e/pageobject/social/activity-tab.po.ts
+++ e2e/pageobject/social/activity-tab.po.ts
@@ -18,7 +18,7 @@ class ActivityTabPage {
         activityLog: '.activity__body .activity-title, .activity__body [style="position: relative;"], .activity__body .field, .activity__body .value',
         filterButton: '.d-icon-filter',
         filterCheckbox: '.checkbox__label span',
-        filterAuthor: '.dropdown input[placeholder="Enter name, email, or login ID"]',
+        filterAuthor: '.dropdown [placeholder="Enter name, email, login ID or employee ID"]',
         filterPopupApplyOrClearButton: '.filter-options button span',
         FilterPopUp: '.bwf-activity-log-filter button[aria-expanded]',
         filterApplyButtonEnableDisabled: '.filter-options button[disabled="disabled"]',
