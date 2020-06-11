7b6d931ddc96171d33ef9d9e146ba71bf1587798
Tushar G <tushar_garud@bmc.com>
2020-06-11 20:12:05 +0530

    task flow fixes

--- e2e/api/api.helper.ts
+++ e2e/api/api.helper.ts
@@ -833,8 +833,8 @@ class ApiHelper {
         let taskSummary = taskTemplateJsonData.fieldInstances[8].value;
         let taskName = taskTemplateJsonData.fieldInstances[1000001437].value;
 
-        oneTaskFlowProcess.flowElements[2].inputMap[0].expression = `"${taskSummary}"`;
-        oneTaskFlowProcess.flowElements[2].inputMap[3].expression = `"${taskTemplateGuid}"`;
+        oneTaskFlowProcess.flowElements[2].inputMap[1].expression = `"${taskSummary}"`;
+        oneTaskFlowProcess.flowElements[2].inputMap[2].expression = `"${taskTemplateGuid}"`;
         oneTaskFlowProcess.layout = (oneTaskFlowProcess.layout).replace("New Task", taskName);
 
         let processGuid = await coreApi.createProcess(oneTaskFlowProcess);
