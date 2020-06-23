0dcf88bbd9fde33b773e1595bcd1a117af8cf809
Garud, Tushar <tushar_garud@bmc.com>
2020-06-23 12:09:05 -0700

    Refactoring (#698)

--- e2e/pageobject/common/update.status.blade.po.ts
+++ e2e/pageobject/common/update.status.blade.po.ts
@@ -13,8 +13,8 @@ class UpdateStatus {
         caseStatusReasonDropDown: '[rx-view-component-id="7128b36c-5d4f-4333-8ee4-2a5163258a45"] button',
         resolutionCodeDropDownGuid: 'fb07b5ff-3c9b-454a-8b0c-a1dfd9987856',
         resolutionCodeRequiredTagGuid: 'f9779ced-7b19-4c6d-ad3a-dfb0acb355d0',
-        saveUpdateStatus: '[rx-view-component-id="ee5dd503-a10e-4d22-9ac5-99c400892bb7"], [rx-view-component-id="6759ba60-df0d-4d5e-8eb9-5101490fd4d4"] button',
-        cancelUpdateStatus: '[rx-view-component-id="7cffd3f8-5b84-4e7f-a4b3-6c0a3dd27855"], [rx-view-component-id="debcdc88-fb42-4003-96d6-1eeb807206b7"] button',
+        saveUpdateStatus: '[rx-view-component-id="ee5dd503-a10e-4d22-9ac5-99c400892bb7"] button, [rx-view-component-id="6759ba60-df0d-4d5e-8eb9-5101490fd4d4"] button',
+        cancelUpdateStatus: '[rx-view-component-id="7cffd3f8-5b84-4e7f-a4b3-6c0a3dd27855"] button, [rx-view-component-id="debcdc88-fb42-4003-96d6-1eeb807206b7"] button',
         statusChange: '.status-transition',
         statusChangeReason: '[rx-view-component-id="049c43a1-4cbd-482d-980d-5db4ed78f295"]',
         searchInput: 'input[type="search"]',
@@ -70,17 +70,12 @@ class UpdateStatus {
     }
 
 
-    async setResolutionDescription(description:string): Promise<void> {
+    async setResolutionDescription(description: string): Promise<void> {
         await $(this.selectors.resolutionDescriptionTextBox).sendKeys(description);
     }
 
     async selectResolutionCode(resolutionCode: string): Promise<void> {
-        let resolutionCodeRequiredTag= await $(`[rx-view-component-id="${this.selectors.resolutionCodeRequiredTagGuid}"]`).$('label').isPresent();
-        if (resolutionCodeRequiredTag==true){
-            await utilityCommon.selectDropDown(this.selectors.resolutionCodeRequiredTagGuid, resolutionCode);
-        }else{
-            await utilityCommon.selectDropDown(this.selectors.resolutionCodeDropDownGuid, resolutionCode);
-        }
+        await utilityCommon.selectDropDown(this.selectors.resolutionCodeRequiredTagGuid, resolutionCode);
     }
 
     async isStatusReasonRequiredTextPresent(): Promise<boolean> {
@@ -97,7 +92,7 @@ class UpdateStatus {
     }
 
     async isSaveUpdateStatusButtonEnabled(): Promise<boolean> {
-        return await element(by.css(this.selectors.saveUpdateStatus)).isEnabled();
+        return await $(this.selectors.saveUpdateStatus).isEnabled();
     }
 
     async isCancelUpdateStatusButtonPresent(): Promise<boolean> {
@@ -123,8 +118,8 @@ class UpdateStatus {
     }
 
     async isRequiredTagToResolutionDescription(): Promise<boolean> {
-        let resolutionDescriptionrequired=  await $('[rx-view-component-id="486a9101-526f-4058-a0e2-3c9e5fab1a36"] label span');
-        return await utilityCommon.isRequiredTagToFieldElement( resolutionDescriptionrequired);
+        let resolutionDescriptionrequired = await $('[rx-view-component-id="486a9101-526f-4058-a0e2-3c9e5fab1a36"] label span');
+        return await utilityCommon.isRequiredTagToFieldElement(resolutionDescriptionrequired);
     }
 
     async getValidationMessage(): Promise<string> {
