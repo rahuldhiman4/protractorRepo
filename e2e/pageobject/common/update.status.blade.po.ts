c8b81260b07cce434febc1c028f87051fe7ab30d
Sahitya, Anant <anant_sahitya@bmc.com>
2020-06-25 22:57:28 +0530

    Fixes Create Case, Bulk Operation and deleteApprovalMapping add (#705)

--- e2e/pageobject/common/update.status.blade.po.ts
+++ e2e/pageobject/common/update.status.blade.po.ts
@@ -65,7 +65,7 @@ class UpdateStatus {
     }
 
     async selectResolutionCode(resolutionCode: string): Promise<void> {
-        await utilityCommon.selectDropDown(this.selectors.resolutionCodeRequiredTagGuid, resolutionCode);
+        await utilityCommon.selectDropDown(this.selectors.resolutionCodeDropDownGuid, resolutionCode);
     }
 
     async isStatusReasonRequiredTextPresent(): Promise<boolean> {
