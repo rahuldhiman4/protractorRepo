0216b6b622880126aaf67f3bcb99b2fa1ed25035
Tushar G <tushar_garud@bmc.com>
2020-08-11 19:27:26 +0530

    common config API, DRDMV-7151 closed

--- e2e/data/api/shared-services/common.configurations.api.ts
+++ e2e/data/api/shared-services/common.configurations.api.ts
@@ -1,27 +1,3 @@
-export const ADD_DWP_SURVEY_ON_CASE = [
-    {
-        "componentName": "Configuration Values",
-        "ownerKeyValue2": "AGGADGG8ECDC0AQBWONEQAYYYH1NA0",
-        "parentComponentName": "Configurations",
-        "settingName": "Configuration Order",
-        "settingValue": "950"
-    },
-    {
-        "componentName": "Configuration Values",
-        "ownerKeyValue2": "AGGADGG8ECDC0AQBWONEQAYYYH1NA0",
-        "parentComponentName": "Configurations",
-        "settingName": "Configuration Value",
-        "settingValue": "true"
-    },
-    {
-        "componentName": "Configuration Values",
-        "ownerKeyValue2": "AGGADGG8ECDC0AQBWONEQAYYYH1NA0",
-        "parentComponentName": "Configurations",
-        "settingName": "Expression",
-        "settingValue": "5a30545b15c828bf11139ffa453419200d69684e9d423ab2f3e869e6bb386507ee9ee24b1252f990cf587177918283e34694939025cd17154380ba49ce43f330"
-    }
-];
-
 export const COMMON_CONFIG_PAYLOAD = [
     {
         "componentName": "Configuration Values",
