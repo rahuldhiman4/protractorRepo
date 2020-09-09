0fd596e437e8a12d68c492366f9344ec96dcb666
Deshmukh, Apurva <apurva_deshmukh_tp@bmc.com>
2020-09-09 12:39:43 +0530

    PIN Validation Create Case  (#935)

---
+++ e2e/tests/case2/pin-validation-create-case.e2e-spec.ts
@@ -0,0 +1,234 @@
+import { browser } from "protractor";
+import apiHelper from "../../api/api.helper";
+import loginPage from "../../pageobject/common/login.po";
+import navigationPage from "../../pageobject/common/navigation.po";
+import { BWF_BASE_URL } from '../../utils/constants';
+import utilityCommon from '../../utils/utility.common';
+import previewCasePo from '../../pageobject/case/case-preview.po';
+import quickCasePo from '../../pageobject/case/quick-case.po';
+import activityTabPo from '../../pageobject/social/activity-tab.po';
+import viewCasePo from '../../pageobject/case/view-case.po';
+import editCasePo from '../../pageobject/case/edit-case.po';
+import resourcesTabPo from '../../pageobject/common/resources-tab.po';
+import createKnowledgePage  from "../../pageobject/knowledge/create-knowlege.po";
+import previewKnowledgePo from '../../pageobject/knowledge/preview-knowledge.po';
+import resourcesPo from '../../pageobject/common/resources-tab.po';
+import createCasePo from '../../pageobject/case/create-case.po';
+import selectCasetemplateBladePo from '../../pageobject/case/select-casetemplate-blade.po';
+import pinValidationPo from '../../pageobject/case/pin-validation.po';
+
+describe('PIN Validation Create Case', () => {
+
+    beforeAll(async () => {
+        await browser.get(BWF_BASE_URL);
+        await loginPage.login('gderuno');
+    });
+
+    afterAll(async () => {
+        await utilityCommon.closeAllBlades();
+        await navigationPage.signOut();
+    });
+
+    describe('[DRDMV-13709,DRDMV-10603,DRDMV-10611,DRDMV-10608,DRDMV-10612]:Case creation via Create Case ,Template validation is ENFORCED', async () => {
+        let casetemplatePsilon1, newCaseTemplate;
+        let randomStr = [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
+        beforeAll(async () => {
+            casetemplatePsilon1 = {
+                "templateName": `${randomStr}1Casetemplate`,
+                "templateStatus": "Draft",
+                "templateSummary": `${randomStr}1Summary`,
+                "caseStatus": "New",
+                "casePriority": "Low",
+                "categoryTier1": 'Purchasing Card',
+                "company": "Psilon",
+                "businessUnit": "Psilon Support Org1",
+                "supportGroup": "Psilon Support Group1",
+                "ownerBU": "Psilon Support Org1",
+                "ownerGroup": "Psilon Support Group1"
+            }
+            await apiHelper.apiLogin('gderuno');
+            newCaseTemplate = await apiHelper.createCaseTemplate(casetemplatePsilon1);
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['ENFORCED'], 'Psilon');
+            await apiHelper.updateCaseTemplateIdentitiyValidation(newCaseTemplate.id, 'ENFORCED');
+            await apiHelper.updateCaseTemplateStatus(newCaseTemplate.id, 'Active');
+            await browser.sleep(3000); // hardwait to reflect case template validation
+        });
+        it('[DRDMV-13709,DRDMV-10603,DRDMV-10611,DRDMV-10608,DRDMV-10612]:Case creation via Create Case ,Template validation is ENFORCED', async () => {
+            await navigationPage.signOut();
+            await loginPage.login('werusha');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeTruthy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("1234");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is unsuccessful.'])).toBeTruthy();
+            await pinValidationPo.clickOnPINCancelBtn();
+            await utilityCommon.closePopUpMessage();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            expect(await previewCasePo.isRequesterNameDisplayed('Gleeu Wixillian')).toBeTruthy();
+            expect(await previewCasePo.isCaseTemplateDisplayed(casetemplatePsilon1.templateName)).toBeTruthy();
+            expect(await previewCasePo.isAssignedCompanyDisplayed('Psilon')).toBeTruthy();
+            expect(await previewCasePo.isRequesterEmailIdDisplayed('gwixillian@psilon.com')).toBeTruthy();
+            await previewCasePo.clickGoToCaseButton();
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        it('[DRDMV-13709,DRDMV-10603,DRDMV-10611,DRDMV-10608,DRDMV-10612]:Case creation via Create Case ,Template validation is ENFORCED', async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['NONE'], 'Psilon');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeTruthy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Requester is not validated for this case.')).toBeFalsy();
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        it('[DRDMV-13709,DRDMV-10603,DRDMV-10611,DRDMV-10608,DRDMV-10612]:Case creation via Create Case ,Template validation is ENFORCED', async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['OPTIONAL'], 'Psilon');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeTruthy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        afterAll(async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await navigationPage.signOut();
+            await loginPage.login('gderuno');
+        });
+    });
+   
+    describe('[DRDMV-10601,DRDMV-10602,DRDMV-10606,DRDMV-10607,DRDMV-10610]:Case creation via Quick Case ,Template validation is OPTIONAL', async () => {
+        let casetemplatePsilon1, newCaseTemplate;
+        let randomStr = [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
+        beforeAll(async () => {
+            casetemplatePsilon1 = {
+                "templateName": `${randomStr}2Casetemplate`,
+                "templateStatus": "Draft",
+                "templateSummary": `${randomStr}2Summary`,
+                "caseStatus": "New",
+                "casePriority": "Low",
+                "categoryTier1": 'Purchasing Card',
+                "company": "Psilon",
+                "businessUnit": "Psilon Support Org1",
+                "supportGroup": "Psilon Support Group1",
+                "ownerBU": "Psilon Support Org1",
+                "ownerGroup": "Psilon Support Group1"
+            }
+            await apiHelper.apiLogin('gderuno');
+            newCaseTemplate = await apiHelper.createCaseTemplate(casetemplatePsilon1);
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['NONE'], 'Psilon');
+            await apiHelper.updateCaseTemplateIdentitiyValidation(newCaseTemplate.id, 'OPTIONAL');
+            await apiHelper.updateCaseTemplateStatus(newCaseTemplate.id, 'Active');
+        });
+        it('[DRDMV-10601,DRDMV-10602,DRDMV-10606,DRDMV-10607,DRDMV-10610]:Case creation via Quick Case ,Template validation is OPTIONAL', async () => {
+            await navigationPage.signOut();
+            await loginPage.login('werusha');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeFalsy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("1234");
+            expect(await utilityCommon.isPopupMsgsMatches([`Requester's PIN not validated for this case.`])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();            
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Requester is not validated for this case.')).toBeTruthy();
+            expect(await activityTabPo.isTextPresentInActivityLog('did not validate the PIN of the requester')).toBeTruthy();
+            await viewCasePo.clickEditCaseButton();
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Requester is not validated for this case.')).toBeTruthy();
+            await editCasePo.clickOnCancelCaseButton();
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeFalsy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();            
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        it('[DRDMV-10601,DRDMV-10602,DRDMV-10606,DRDMV-10607,DRDMV-10610]:Case creation via Quick Case ,Template validation is OPTIONAL', async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['OPTIONAL'], 'Psilon');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeFalsy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("1234");
+            expect(await utilityCommon.isPopupMsgsMatches([`Requester's PIN not validated for this case.`])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();          
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Requester is not validated for this case.')).toBeTruthy();
+            expect(await activityTabPo.isTextPresentInActivityLog('did not validate the PIN of the requester')).toBeTruthy();
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeFalsy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();           
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        it('[DRDMV-10601,DRDMV-10602,DRDMV-10606,DRDMV-10607,DRDMV-10610]:Case creation via Quick Case ,Template validation is OPTIONAL', async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await apiHelper.addCommonConfig('IDENTITY_VALIDATION', ['ENFORCED'], 'Psilon');
+            await navigationPage.gotoCreateCase();
+            await createCasePo.selectRequester('gwixillian');
+            await createCasePo.setSummary('Summary' + randomStr);
+            await createCasePo.clickSelectCaseTemplateButton();
+            await selectCasetemplateBladePo.selectCaseTemplate(casetemplatePsilon1.templateName);
+            expect(await createCasePo.isSaveCaseButtonDisabled()).toBeTruthy('Save button Enabled');
+            expect(await pinValidationPo.isIdentityValidationMessageDisplayed('Please validate the requester.')).toBeTruthy();
+            await pinValidationPo.validatePin("12345");
+            expect(await utilityCommon.isPopupMsgsMatches(['Validation of requester is successful.'])).toBeTruthy();
+            await createCasePo.clickSaveCaseButton();
+            await previewCasePo.clickGoToCaseButton();          
+            expect(await activityTabPo.isTextPresentInActivityLog('validated the PIN of the requester')).toBeTruthy();
+        });
+        afterAll(async () => {
+            await apiHelper.apiLogin('tadmin');
+            await apiHelper.deleteCommonConfig('IDENTITY_VALIDATION', 'Psilon');
+            await navigationPage.signOut();
+            await loginPage.login('gderuno');
+        });
+    });
+});
