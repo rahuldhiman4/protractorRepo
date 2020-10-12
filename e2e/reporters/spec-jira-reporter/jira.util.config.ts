8e390e2a85122c09454966880a2e3569bee05a2c
Garud, Tushar <tushar_garud@bmc.com>
2020-10-12 16:21:11 +0530

    Push1 execution changes (#1065)

--- e2e/reporters/spec-jira-reporter/jira.util.config.ts
+++ e2e/reporters/spec-jira-reporter/jira.util.config.ts
@@ -1,10 +1,10 @@
 export const TEST_CYCLE_NAME = "Automated:UI";
 export const TEST_CYCLE_SUBFOLDER_NAME = "BWF:UI_AutoRun";
-export const BUILD = "20.08.00-SNAPSHOT";
+export const BUILD = "20.08.01-SNAPSHOT";
 export const ENV = "AutomationStack";
 export const BASE_URI = "http://jiraapi.bmc.com:8080/";
 export const PROJECT_ID = "11900";
-export const VERSION_ID = "40509";
+export const VERSION_ID = "47800";
 export const INPUT_FILE = "../../reports/spec-json-report/spec-json-report.json";
 export const GENERATE_STATS = "false";
 export const USERNAME = "bwfjira";
