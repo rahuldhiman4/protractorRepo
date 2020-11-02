deace51f97eed8a104ad50e03b9f195cbb2254d5
Garud, Tushar <tushar_garud@bmc.com>
2020-11-02 11:59:06 +0530

    incoming email config (#1174)

--- e2e/data/api/email/email.configuration.data.api.ts
+++ e2e/data/api/email/email.configuration.data.api.ts
@@ -4,11 +4,15 @@ export const MAILBOX_CONFIG = {
     "fieldInstances": {
         "8": {
             "id": 8,
-            "value": "test email config"
+            "value": "test desc"
+        },
+        "450000152": {
+            "id": 450000152,
+            "value": "IncomingMail"
         },
         "450000156": {
             "id": 450000156,
-            "value": "bmctemptestemail@gmail.com"
+            "value": "test@gmail.com"
         },
         "450000420": {
             "id": 450000420,
@@ -19,4 +23,43 @@ export const MAILBOX_CONFIG = {
             "value": "5a30545b15c828bf11139ffa453419200d69684e9d423ab2f3e869e6bb386507ee9ee24b1252f990cf587177918283e34694939025cd17154380ba49ce43f330"
         }
     }
-}
+};
+
+export const INCOMINGMAIL_DEFAULT = {
+    "resourceType": "com.bmc.arsys.rx.services.record.domain.RecordInstance",
+    "recordDefinitionName": "AR System Email Mailbox Configuration",
+    "fieldInstances": {
+        "123": {
+            "id": 123,
+            "value": "Bmc@12345"
+        },
+        "18037": {
+            "id": 18037,
+            "value": "IncomingMail"
+        },
+        "18038": {
+            "id": 18038,
+            "value": 995
+        },
+        "18039": {
+            "id": 18039,
+            "value": "bmctemptestemail@gmail.com"
+        },
+        "18046": {
+            "id": 18046,
+            "value": 1
+        },
+        "18050": {
+            "id": 18050,
+            "value": 1
+        },
+        "18130": {
+            "id": 18130,
+            "value": "pop.gmail.com"
+        },
+        "18217": {
+            "id": 18217,
+            "value": "Temp"
+        }
+    }
+};
