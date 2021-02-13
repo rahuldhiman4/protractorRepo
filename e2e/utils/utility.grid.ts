83274003195a703c779a43f47a61949f97876e9b
Gaikwad, Kiran <kiran_gaikwad_tp@bmc.com>
2021-02-13 17:42:51 +0530

    Person Profile (#1543)

--- e2e/utils/utility.grid.ts
+++ e2e/utils/utility.grid.ts
@@ -163,7 +163,7 @@ export class GridOperations {
             if (result) {
                 await $(filterPresetBtn).click();
                 await $$(clearBtn).first().click();
-                await $(filterPresetBtn).click(); //Need to update once defect DRDMV-24648 is resolved
+                await $(refreshIcon).click();
                 let hiddentFilter2 = await $('.adapt-table-toolbar-hidden-items-dropdown .d-icon-ellipsis').isPresent();
                 if (hiddentFilter2 == true) {
                     await $('.adapt-table-toolbar-hidden-items-dropdown .d-icon-ellipsis').click();
