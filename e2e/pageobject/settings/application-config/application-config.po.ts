e04ea32715b2d1c8dbbc268a3986d62e9cf4a32d
Tushar G <tushar_garud@bmc.com>
2021-03-05 14:38:36 +0530

    app config fix

--- e2e/pageobject/settings/application-config/application-config.po.ts
+++ e2e/pageobject/settings/application-config/application-config.po.ts
@@ -1,20 +1,26 @@
-import { $, browser, by, element, protractor, ProtractorExpectedConditions, $$ } from "protractor";
+import { $, by, element, protractor, ProtractorExpectedConditions } from "protractor";
+import { DropDownType } from '../../../utils/constants';
+import utilityCommon from '../../../utils/utility.common';
 
 class ApplicationConfiguration {
     EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
     selectors = {
-        applicationConfiguration: 'ul[class="list-unstyled"] span',
+        applicationConfigurationLink: 'a.no-href-link',
         selectConfigurationDropDownArrow: 'span[class="arrow-icon d-icon-right-angle_down"]',
         configurationDefaultValue: '[class="d-textfield_required d-textfield"] input',
         addConfigurationValueButton: '[class="col-md-8"] span[class="d-icon-left-plus_circle"]',
         configurationValueText: '.form-group input',
-        companyDropDown: 'button[role="listbox"]',
-        companyDropdownList: 'button.dropdown-item',
         saveButton: 'div[class="float-right"] button[btn-type="primary"]',
         cancelButton: 'div[class="float-right"] button[btn-type="secondary"]',
         removeButton: '.d-icon-left-cross',
         columnValue: '[class="card-title rx-ellipsis"]',
         configurationHeaderValue: 'div[class="col-md-8"] div[class="header"]',
+        companyDropDown: '[rx-view-component-id="31c2f4cd-d23a-434c-843d-cabe10afa08b"] button',
+    }
+
+    async isCompanyListMatches(companyList: string[]): Promise<void> {
+        let companyDDElement = await $(this.selectors.companyDropDown);
+        await utilityCommon.isAllDropDownValuesMatches(companyDDElement, companyList, DropDownType.WebElement);
     }
 
     async setConfigurationValueText(value: string): Promise<void> {
@@ -29,13 +35,13 @@ class ApplicationConfiguration {
         return await $(this.selectors.configurationHeaderValue).getText();
     }
 
-    async selectCompany(value: string): Promise<void> {
-        await $(this.selectors.companyDropDown).click();
-        await element(by.cssContainingText(this.selectors.companyDropdownList, value)).click();
+    async selectCompany(companyValue: string): Promise<void> {
+        let companyDDElement = await $(this.selectors.companyDropDown);
+        await utilityCommon.selectDropDown(companyDDElement, companyValue, DropDownType.WebElement);
     }
 
     async clickApplicationConfiguration(value: string): Promise<void> {
-        await element(by.cssContainingText(this.selectors.applicationConfiguration, value)).click();
+        await element(by.cssContainingText(this.selectors.applicationConfigurationLink, value)).click();
     }
 
     async clickConfigurationDropDownArrow(): Promise<void> {
