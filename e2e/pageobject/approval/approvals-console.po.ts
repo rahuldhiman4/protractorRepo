05d2a719cc729b71ecc4f7544fb91bd04cb4047e
Shastrakar, Akshay <akshay_shastrakar_tp@bmc.com>
2021-03-10 19:38:20 +0530

    [approval] case-apporval-ui-validation (#1694)

--- e2e/pageobject/approval/approvals-console.po.ts
+++ e2e/pageobject/approval/approvals-console.po.ts
@@ -1,4 +1,4 @@
-import { browser, $, protractor, ProtractorExpectedConditions } from "protractor";
+import { browser, $, $$, protractor, ProtractorExpectedConditions } from "protractor";
 import utilityGrid from '../../utils/utility.grid';
 import utilityCommon from '../../utils/utility.common';
 
@@ -10,6 +10,7 @@ class ApprovalsConsole {
         approveBtn: 'button.approve-btn',
         rejectBtn: 'button.reject-btn',
         holdBtn: 'button.hold-btn',
+        confirmButton: '[rx-id="confirm-button"]'
     }
 
 
@@ -25,8 +26,13 @@ class ApprovalsConsole {
         await $(this.selectors.holdBtn).click();
     }
 
+    async clickConfirmButton() {
+        await $(this.selectors.confirmButton).click();
+    }
+
     async searchCaseOnApprovalConsole(caseSummary: string, approve: string): Promise<void> {
-        await utilityGrid.searchAndSelectGridRecord(caseSummary);
+        // await utilityGrid.searchAndSelectGridRecord(caseSummary);
+        await this.searchAndSelectGridRecord(caseSummary);
         switch (approve) {
             case "Approve": {
                 await this.clickApproverButton();
@@ -45,7 +51,43 @@ class ApprovalsConsole {
                 break;
             }
         }
-        await utilityCommon.clickOnApplicationWarningYesNoButton('Yes');
+        // await utilityCommon.clickOnApplicationWarningYesNoButton('Yes'); // commented because old warning popup present
+        await this.clickConfirmButton();
+    }
+
+    // methods of 20.08.01 for approval record grid
+    async searchAndSelectGridRecord(recordName: string, guid?: string): Promise<void> {
+        let selectCheckbox = '.clickable .ui-grid-selection-row-header-buttons';
+        let selectRadioButton = '.radio__label input';
+        if (guid) {
+            await this.searchRecordWithoutFilter(recordName, guid);
+            selectCheckbox = `[rx-view-component-id="${guid}"] ` + selectCheckbox;
+            selectRadioButton = `[rx-view-component-id="${guid}"] ` + selectRadioButton;
+        }
+        else await this.searchRecordWithoutFilter(recordName);
+        let checkboxLocator = await $(selectCheckbox);
+        let radioButtonLocator = await $(selectRadioButton);
+        if (await checkboxLocator.isPresent()) await $$(selectCheckbox).first().click();
+        else await radioButtonLocator.click();
+    }
+
+    // methods of 20.08.01 for approval record grid
+    async searchRecordWithoutFilter(searchValue: string, guid?: string): Promise<void> {
+        let searchTextBoxLocator: string = '.d-textfield__label input';
+        let gridRecordsLocator: string = '.at-data-row';
+        if (guid) {
+            searchTextBoxLocator = `[rx-view-component-id="${guid}"] ` + searchTextBoxLocator;
+            gridRecordsLocator = `[rx-view-component-id='${guid}'] ` + gridRecordsLocator;
+        }
+        for (let i: number = 0; i < 2; i++) {
+            console.log(searchValue, "search angular grid count: ", i);
+            await $(searchTextBoxLocator).clear();
+            await $(searchTextBoxLocator).sendKeys(searchValue + protractor.Key.ENTER);
+            let gridRecordCount: number = await $$(gridRecordsLocator).count();
+            if (gridRecordCount == 0) {
+                await browser.sleep(5000); // workaround for performance issue, this can be removed when issue fixed
+            } else break;
+        }
     }
 }
 
